f00e2fa6b84b08cfc0ba2ce39dd58123dde7a199
ajaysreedhar <sree.ajay468@gmail.com>
2026-08-16 00:23:18 +0530

    fix: append sources path to include_directory list

--- scripts/shared-utils.cmake
+++ scripts/shared-utils.cmake
@@ -3,10 +3,10 @@
 # Turns every .c/.s file in the calling directory into an OBJECT
 # library called <name>, and registers it in the global AARC_SYSTEM_OBJECTS
 # list so the top-level build picks it up automatically.
-function(xadd_system_object obj_name)
+function(xadd_system_object obj_name source_path)
     file(GLOB_RECURSE obj_sources CONFIGURE_DEPENDS
-        "${CMAKE_CURRENT_SOURCE_DIR}/${obj_name}/*.c"
-        "${CMAKE_CURRENT_SOURCE_DIR}/${obj_name}/*.s"
+        "${CMAKE_CURRENT_SOURCE_DIR}/${source_path}/*.c"
+        "${CMAKE_CURRENT_SOURCE_DIR}/${source_path}/*.s"
     )
 
     if(NOT obj_sources)
@@ -19,7 +19,7 @@ function(xadd_system_object obj_name)
         $<$<COMPILE_LANGUAGE:C>:-Wall -O2 -ffreestanding -nostdinc -nostdlib -nostartfiles>)
 
     target_include_directories(${obj_name} PUBLIC 
-        ${CMAKE_CURRENT_SOURCE_DIR} 
+        ${CMAKE_CURRENT_SOURCE_DIR}/${source_path} 
         ${CMAKE_SOURCE_DIR}/src/include)
 
     set_property(GLOBAL APPEND PROPERTY AARC_SYSTEM_OBJECTS ${obj_name})
