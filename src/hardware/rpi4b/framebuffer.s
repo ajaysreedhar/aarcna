d3585005ae3d5c5b2d0395bf6406877e83cda650
ajaysreedhar <sree.ajay468@gmail.com>
2026-08-19 01:08:59 +0530

    tempfix: use indexed bit masking to copy words

--- src/hardware/rpi4b/framebuffer.s
+++ src/hardware/rpi4b/framebuffer.s
@@ -41,34 +41,34 @@ framebuffer_init:
     MOVZ    W10, #(35 * 4)                      // Lower 32 bits => Index 0
     MOVK    X10, #MBOX_REQUEST_CODE, LSL #32    // Upper 32 bits => Index 1
     MOVZ    W11, #(2 * 4)                               // Lower 32 bits => Index 2
-    MOVK    X11, #(MBOX_TAG_SETPHYWH | 0xFFFF), LSL #32 // Upper 32 bits => Index 3
+    MOVK    X11, #(MBOX_TAG_SETPHYWH & 0xFFFF), LSL #32 // Upper 32 bits => Index 3
     MOVK    X11, #(MBOX_TAG_SETPHYWH >> 16), LSL #48
     STP     X10, X11, [X9]
 
     MOV     W10, WZR          // Lower 32 bits => Index 4
-    MOVK    X10, X0, LSL #32  // Upper 32 bits => Index 5
-    MOVZ    W11, W1                                      // Lower 32 bits => Index 6
-    MOVK    X11, #(MBOX_TAG_SETVIRTWH | 0xFFFF), LSL #32 // Upper 32 bits => Index 7
+    BFI     X10, X0, #32, #32 // Upper 32 bits => Index 5
+    MOV     W11, W1                                      // Lower 32 bits => Index 6
+    MOVK    X11, #(MBOX_TAG_SETVIRTWH & 0xFFFF), LSL #32 // Upper 32 bits => Index 7
     MOVK    X11, #(MBOX_TAG_SETVIRTWH >> 16), LSL #48
     STP     X10, X11, [X9, #16]
 
-    MOVZ    W10, #(1 * 4)     // Lower 32 bits => Index 8
-    MOVK    X10, XZR, LSL #32 // Upper 32 bits => Index 9
-    MOVZ    W11, W0           // Lower 32 bits => Index 10
-    MOVK    X11, X1, LSL #32  // Upper 32 bits => Index 11
+    MOVZ    W10, #(2 * 4)          // Lower 32 bits => Index 8
+    MOVK    X10, #(2 * 4), LSL #32 // Upper 32 bits => Index 9
+    MOV     W11, W0                // Lower 32 bits => Index 10
+    BFI     X11, X1, #32, #32      // Upper 32 bits => Index 11
     STP     X10, X11, [X9, #32]
 
-    MOVZ    X10, #(MBOX_TAG_SETDEPTH | 0xFFFF)        // Lower 32 bits => Index 12
+    MOVZ    W10, #(MBOX_TAG_SETDEPTH & 0xFFFF)        // Lower 32 bits => Index 12
     MOVK    X10, #(MBOX_TAG_SETPHYWH >> 16), LSL #16
     MOVK    X10, 1 * 4, LSL #32                       // Upper 32 bits => Index 13
-    MOVZ    W11, WZR                                  // Lower 32 bits => Index 14
-    MOVK    X11, X2, LSL #32                          // Upper 32 bits => Index 15
+    MOV     W11, WZR                                  // Lower 32 bits => Index 14
+    BFI     X11, X2, #32, #32                         // Upper 32 bits => Index 15
     STP     X10, X11, [X9, #48]
 
-    MOVZ    X10, #(MBOX_TAG_ALLOCBUF | 0xFFFF)        // Lower 32 bits => Index 16
+    MOVZ    X10, #(MBOX_TAG_ALLOCBUF & 0xFFFF)        // Lower 32 bits => Index 16
     MOVK    X10, #(MBOX_TAG_ALLOCBUF >> 16), LSL #16
     MOVK    X10, #8, LSL #32                          // Upper 32 bits => Index 17
-    MOVZ    W11, WZR                                  // Lower 32 bits => Index 18
+    MOV     W11, WZR                                  // Lower 32 bits => Index 18
     MOVK    X11, #16, LSL #32                         // Upper 32 bits => Index 19
     MOVK    X11, #(MBOX_TAG_SETPHYWH >> 16), LSL #48
     STP     X10, X11, [X9, #64]
@@ -76,20 +76,20 @@ framebuffer_init:
     MOVZ    X10, #(35 * 4)                      // Lower 32 bits => Index 20
     MOVK    X10, #MBOX_REQUEST_CODE, LSL #32    // Upper 32 bits => Index 21
     MOVZ    X11, #(2 * 4)                               // Lower 32 bits => Index 22
-    MOVK    X11, #(MBOX_TAG_SETPHYWH | 0xFFFF), LSL #32 // Upper 32 bits => Index 23
+    MOVK    X11, #(MBOX_TAG_SETPHYWH & 0xFFFF), LSL #32 // Upper 32 bits => Index 23
     MOVK    X11, #(MBOX_TAG_SETPHYWH >> 16), LSL #48
     STP     X10, X11, [X9, #80]
 
     MOVZ    X10, #(35 * 4)                      // Lower 32 bits => Index 24
     MOVK    X10, #MBOX_REQUEST_CODE, LSL #32    // Upper 32 bits => Index 25
     MOVZ    X11, #(2 * 4)                               // Lower 32 bits => Index 26
-    MOVK    X11, #(MBOX_TAG_SETPHYWH | 0xFFFF), LSL #32 // Upper 32 bits => Index 27
+    MOVK    X11, #(MBOX_TAG_SETPHYWH & 0xFFFF), LSL #32 // Upper 32 bits => Index 27
     MOVK    X11, #(MBOX_TAG_SETPHYWH >> 16), LSL #48
     STP     X10, X11, [X9, #96]
 
     MOVZ    W10, #4096                             // Lower 32 bits => Index 28
     // ZERO                                          // Upper 32 bits => Index 29
-    MOVK    X11, #(MBOX_TAG_SETPHYWH | 0xFFFF)       // Upper 32 bits => Index 30
+    MOVK    X11, #(MBOX_TAG_SETPHYWH & 0xFFFF)       // Upper 32 bits => Index 30
     MOVK    X11, #(MBOX_TAG_SETPHYWH >> 16), LSL #16
     MOVK    X11, #4, LSL #32                         // Upper 32 bits => Index 31
     STP     X10, X11, [X9, #112]
