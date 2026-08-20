d40b16c8cab114afa8ccfe8fa6b1842a064d333d
ajaysreedhar <sree.ajay468@gmail.com>
2026-08-21 00:56:54 +0530

    chore: convert GPU address to ARM64 address

--- src/hardware/rpi4b/framebuffer.s
+++ src/hardware/rpi4b/framebuffer.s
@@ -1,17 +1,12 @@
+.include "mailbox.inc.s"
+
 .type mailbox_buffer, %object
 .global mailbox_buffer
 
 .global framebuffer_init
 .type framebuffer_init, %function
 
-.equ MBOX_REQUEST_CODE, 0x0
-
-.equ MBOX_TAG_SETPHYWH,  0x00048003 // Set physical width/height
-.equ MBOX_TAG_SETVIRTWH, 0x00048004 // Set virtual width/height
-.equ MBOX_TAG_SETDEPTH,  0x00048005 // Set depth (bits per pixel)
-.equ MBOX_TAG_ALLOCBUF,  0x00040001 // Allocate framebuffer
-.equ MBOX_TAG_GETPITCH,  0x00040008 // Get pitch (bytes per line)
-.equ MBOX_TAG_LAST,      0x00000000 // End tag
+.extern mailbox_call
 
 /* Executable instructions. */
 .section .text
@@ -28,7 +23,11 @@
  *   W2 - depth.
  * 
  * Results:
- *   W0 - Just zero.
+ *   X0 - The framebuffer address.
+ *   W1 - Actual physical width.
+ *   W2 - Actual physical height.
+ *   W3 - Number of bytes per line.
+ *   W4 - Pixel order.
  * ============================================================
  */
 framebuffer_init:
@@ -38,11 +37,13 @@ framebuffer_init:
     ADRP    X9, mailbox_buffer
     ADD     X9, X9, :lo12:mailbox_buffer
 
+    STR     X9, [SP, #16] // Store the mailbox_buffer address.
+
     MOVZ    W10, #(35 * 4)                      // Lower 32 bits => Index 0
     MOVK    X10, #MBOX_REQUEST_CODE, LSL #32    // Upper 32 bits => Index 1
-    MOVZ    W11, #(2 * 4)                               // Lower 32 bits => Index 2
-    MOVK    X11, #(MBOX_TAG_SETPHYWH & 0xFFFF), LSL #32 // Upper 32 bits => Index 3
-    MOVK    X11, #(MBOX_TAG_SETPHYWH >> 16), LSL #48
+    MOVZ    W11, #(MBOX_TAG_SETPHYWH & 0xFFFF)        // Lower 32 bits => Index 2
+    MOVK    W11, #(MBOX_TAG_SETPHYWH >> 16), LSL #16
+    MOVK    X11, #8, LSL #32                          // Upper 32 bits => Index 3
     STP     X10, X11, [X9]
 
     MOV     W10, WZR          // Lower 32 bits => Index 4
@@ -52,54 +53,80 @@ framebuffer_init:
     MOVK    X11, #(MBOX_TAG_SETVIRTWH >> 16), LSL #48
     STP     X10, X11, [X9, #16]
 
-    MOVZ    W10, #(2 * 4)          // Lower 32 bits => Index 8
-    MOVK    X10, #(2 * 4), LSL #32 // Upper 32 bits => Index 9
-    MOV     W11, W0                // Lower 32 bits => Index 10
-    BFI     X11, X1, #32, #32      // Upper 32 bits => Index 11
+    MOVZ    W10, #8             // Lower 32 bits => Index 8
+    MOVK    X10, #8, LSL #32    // Upper 32 bits => Index 9
+    MOV     W11, W0             // Lower 32 bits => Index 10
+    BFI     X11, X1, #32, #32   // Upper 32 bits => Index 11
     STP     X10, X11, [X9, #32]
 
-    MOVZ    W10, #(MBOX_TAG_SETDEPTH & 0xFFFF)        // Lower 32 bits => Index 12
-    MOVK    X10, #(MBOX_TAG_SETPHYWH >> 16), LSL #16
-    MOVK    X10, 1 * 4, LSL #32                       // Upper 32 bits => Index 13
-    MOV     W11, WZR                                  // Lower 32 bits => Index 14
-    BFI     X11, X2, #32, #32                         // Upper 32 bits => Index 15
+    MOVZ    W10, #(MBOX_TAG_SETVIRTOFF & 0xFFFF)       // Lower 32 bits => Index 12
+    MOVK    X10, #(MBOX_TAG_SETVIRTOFF >> 16), LSL #16
+    MOVK    X10, #8, LSL #32                           // Upper 32 bits => Index 13
+    MOVZ    W11, #8                                    // Lower 32 bits => Index 14
+    // Zero                                               Upper 32 bits => Index 15
     STP     X10, X11, [X9, #48]
 
-    MOVZ    X10, #(MBOX_TAG_ALLOCBUF & 0xFFFF)        // Lower 32 bits => Index 16
-    MOVK    X10, #(MBOX_TAG_ALLOCBUF >> 16), LSL #16
-    MOVK    X10, #8, LSL #32                          // Upper 32 bits => Index 17
-    MOV     W11, WZR                                  // Lower 32 bits => Index 18
-    MOVK    X11, #16, LSL #32                         // Upper 32 bits => Index 19
-    MOVK    X11, #(MBOX_TAG_SETPHYWH >> 16), LSL #48
+    MOV     W10, WZR                                   // Lower 32 bits => Index 16
+    MOVK    X10, #(MBOX_TAG_SETDEPTH & 0xFFFF), LSL 32 // Upper 32 bits => Index 17
+    MOVK    X10, #(MBOX_TAG_SETDEPTH >> 16), LSL #48
+    MOVZ    W11, #4                                    // Lower 32 bits => Index 18
+    MOVK    X11, #4, LSL #32                           // Upper 32 bits => Index 19
     STP     X10, X11, [X9, #64]
 
-    MOVZ    X10, #(35 * 4)                      // Lower 32 bits => Index 20
-    MOVK    X10, #MBOX_REQUEST_CODE, LSL #32    // Upper 32 bits => Index 21
-    MOVZ    X11, #(2 * 4)                               // Lower 32 bits => Index 22
-    MOVK    X11, #(MBOX_TAG_SETPHYWH & 0xFFFF), LSL #32 // Upper 32 bits => Index 23
-    MOVK    X11, #(MBOX_TAG_SETPHYWH >> 16), LSL #48
+    MOVZ    W10, #32                                      // Lower 32 bits => Index 20
+    MOVK    X10, #(MBOX_TAG_SETPXLORDR & 0xFFFF), LSL #32 // Upper 32 bits => Index 21
+    MOVK    X10, #(MBOX_TAG_SETPXLORDR >> 16), LSL #48
+    MOVZ    W11, #4             // Lower 32 bits => Index 22
+    MOVK    X11, #4, LSL #32    // Upper 32 bits => Index 23
     STP     X10, X11, [X9, #80]
 
-    MOVZ    X10, #(35 * 4)                      // Lower 32 bits => Index 24
-    MOVK    X10, #MBOX_REQUEST_CODE, LSL #32    // Upper 32 bits => Index 25
-    MOVZ    X11, #(2 * 4)                               // Lower 32 bits => Index 26
-    MOVK    X11, #(MBOX_TAG_SETPHYWH & 0xFFFF), LSL #32 // Upper 32 bits => Index 27
-    MOVK    X11, #(MBOX_TAG_SETPHYWH >> 16), LSL #48
+    MOV     W10, WZR                                    // Lower 32 bits => Index 24
+    MOVK    X10, #(MBOX_TAG_ALLOCBUF & 0xFFFF), LSL #32 // Upper 32 bits => Index 25
+    MOVK    X10, #(MBOX_TAG_ALLOCBUF >> 16), LSL #48
+    MOVZ    W11, #8           // Lower 32 bits => Index 26
+    MOVK    X11, #8, LSL #32  // Upper 32 bits => Index 27
     STP     X10, X11, [X9, #96]
 
-    MOVZ    W10, #4096                             // Lower 32 bits => Index 28
+    MOVZ    X10, #4096                             // Lower 32 bits => Index 28
     // ZERO                                          // Upper 32 bits => Index 29
-    MOVK    X11, #(MBOX_TAG_SETPHYWH & 0xFFFF)       // Upper 32 bits => Index 30
-    MOVK    X11, #(MBOX_TAG_SETPHYWH >> 16), LSL #16
+    MOVZ    W11, #(MBOX_TAG_GETPITCH & 0xFFFF)       // Lower 32 bits => Index 30
+    MOVK    W11, #(MBOX_TAG_GETPITCH >> 16), LSL #16
     MOVK    X11, #4, LSL #32                         // Upper 32 bits => Index 31
     STP     X10, X11, [X9, #112]
 
-    MOVZ    X10, #4           // Lower 32 bits => Index 32
+    MOVZ    W10, #4           // Lower 32 bits => Index 32
     // Zero! #MBOX_TAG_LAST,  // Upper 32 bits => Index 33
     MOV     X11, XZR          // Index 34, 35 zeroed out.
     STP     X10, X11, [X9, #128]
 
-    MOV     SP, X29
+    MOV     W0, #MBOX_CH_PROPS
+    MOV     X1, X9
+    BL      mailbox_call
+    CBZ     W0, 1f
+
+    LDR     X9, [SP, #16] // Load the mailbox_buffer address.
+
+    MOVZ    W10, #32
+    LDR     W11, [X9, #80] // mailbox_buffer[20]
+    CMP     W10, W11
+    BNE     1f
+
+    LDR     W11, [X9, #112] // mailbox_buffer[28]
+    CBZ     1f
+
+    /* To convert GPU address to ARM address, AND with 0x3FFFFFFF */
+    MOV     X10, XZR
+    MVN     W10, WZR
+    BFC     W10, #30, #2
+    AND     X0, X10, X11
+    STR     X0, [X9, #112] // mailbox_buffer[28]
+
+    LDR     W1, [X9, #40]  // mailbox_buffer[10]
+    LDR     W2, [X9, #44]  // mailbox_buffer[11]
+    LDR     W3, [X9, #132] // mailbox_buffer[33]
+    LDR     W4, [X9, #96]    // mailbox_buffer[24]
+
+1:  MOV     SP, X29
     LDP     X29, X30, [SP], #32
 
     RET
